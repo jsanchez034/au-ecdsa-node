280180efe44862d8c972a82203ad9e771a8ad7c0
JP <jp@animoto.com>
2023-03-16 07:01:28 -0400

    add api baseURL

--- client/src/server.js
+++ client/src/server.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const server = axios.create({
-  baseURL: process.env.NODE_ENV === 'production' ? "/api" : "http://localhost:3042/api",
+  baseURL: process.env.NODE_ENV === 'production' ? "https://au-ecdsa-node-pvu9.vercel.app/api" : "http://localhost:3042/api",
 });
 
 export default server;
